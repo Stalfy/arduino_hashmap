6130772e8f5b91d74fe9a7dc1964837b6d3cc59c
Cédric Sauriol <cedric.sauriol@hotmail.com>
2019-10-25 09:58:48 -0400

    Added a default hashing function for the integer types up to 32 bits.

--- HashMap/keyhash.h
+++ HashMap/keyhash.h
@@ -1,3 +1,7 @@
+// Default operation assumes a uint32_t. 
+// Based on the result from the hash prospector.
+// https://github.com/skeeto/hash-prospector
+
 #ifndef KEYHASH_H_
 #define KEYHASH_H_
 
@@ -5,9 +9,22 @@
 
 template <typename K>
 class KeyHash {
-public:
-  KeyHash() {};
-  virtual uint32_t operator() (K key) = 0;
+    public:
+        KeyHash() {};
+        uint32_t operator() (K key) {
+            // exact bias: 0.020888578919738908
+
+            uint32_t x = reinterpret_cast<uint32_t>(key);
+            x ^= x >> 17;
+            x *= reinterpret_cast<uint32_t>(0xed5ad4bb);
+            x ^= x >> 11;
+            x *= reinterpret_cast<uint32_t>(0xac4c1b51);
+            x ^= x >> 15;
+            x *= reinterpret_cast<uint32_t>(0x31848bab);
+            x ^= x >> 14;
+
+            return x;
+        };
 };
 
 #endif
